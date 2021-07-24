187906174abfd7d3164929e1c3f3e031d59bb683
Dariel90 <dariel901231@gmail.com>
2021-07-23 20:50:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Tesseract;
+
+namespace tesseract_ocr_demo
+{
+    class Program
+    {
+        private const string TestImagePath = "Ocr/phototest.tif";
+        private const string TeicketFoto = "Ocr/ticket-solo-sin-letras.jpg";
+        
+        private const string Ticket = "Ocr/Ticket1.png";
+        static void Main(string[] args)
+        {
+            var result = CanParseText();
+            Console.WriteLine("Devuelve el texto de una imagen");
+            Console.WriteLine(result);
+            //var result2 = CanProcessSpecifiedRegionInImage();
+            //Console.WriteLine(result2);
+            //Console.ReadLine();
+        }
+
+        public static string CanParseText()
+        {
+            using (var engine = TEngine.CreateEngine())
+            {
+                using (var img = TEngine.LoadTestPix(TeicketFoto))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        var text = page.GetText();
+
+                        //const string expectedText =
+                        //    "This is a lot of 12 point text to test the\nocr code and see if it works on all types\nof file format.\n\nThe quick brown dog jumped over the\nlazy fox. The quick brown dog jumped\nover the lazy fox. The quick brown dog\njumped over the lazy fox. The quick\nbrown dog jumped over the lazy fox.\n";
+
+                        return text;
+                    }
+                }
+            }
+        }
+
+        public static string CanProcessSpecifiedRegionInImage()
+        {
+            using (var engine = TEngine.CreateEngine(mode: EngineMode.LstmOnly))
+            {
+                using (var img = TEngine.LoadTestPix(TestImagePath))
+                {
+                    using (var page = engine.Process(img, Rect.FromCoords(0, 0, img.Width, 188)))
+                    {
+                        var region1Text = page.GetText();
+
+                        //const string expectedTextRegion1 =
+                        //    "This is a lot of 12 point text to test the\nocr code and see if it works on all types\nof file format.\n";
+
+                        return region1Text;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TesseractEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace tesseract_ocr_demo
+{
+    public static class TEngine
+    {
+        public static TesseractEngine CreateEngine(string lang = "eng", EngineMode mode = EngineMode.Default)
+        {
+            var datapath = DataPath;
+            return new TesseractEngine(datapath, lang, mode);
+        }
+
+        private static string DataPath
+        {
+            get { return AbsolutePath("tessdata"); }
+        }
+        private static string AbsolutePath(string relativePath)
+        {
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            return Path.Combine(Path.GetDirectoryName(path), relativePath);
+        }
+        public static Pix LoadTestPix(string filename)
+        {
+            var testFilename = TestFilePath(filename);
+            return Pix.LoadFromFile(testFilename);
+        }
+        private static string TestFilePath(string path)
+        {
+            var basePath = AbsolutePath("Data");
+
+            return Path.GetFullPath(Path.Combine(basePath, path));
+        }
+    }
+}
